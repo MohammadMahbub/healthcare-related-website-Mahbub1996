a400f4ffb05a7e36f2d86ee164d2f94b57bbc923
Mahbub1996 <mdmahbubur511@gmail.com>
2021-10-19 19:58:02 +0600

    assignment complete

---
+++ src/Components/PatientLoginPage/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlrRaiCkCu4ToG5So9nLJO6saD1e6VQ0s",
+    authDomain: "dental-care-5ad47.firebaseapp.com",
+    projectId: "dental-care-5ad47",
+    storageBucket: "dental-care-5ad47.appspot.com",
+    messagingSenderId: "860529670833",
+    appId: "1:860529670833:web:db77e50cd1d2ab37cc8090"
+  };
+
+  export default firebaseConfig;
---
+++ src/Components/ServicesPage/SingleService.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SingleService = props => {
+    const { title, image, description } = props.data;
+    return (
+        <div className="">
+            <div className="single-service">
+                <h3> {title} </h3>
+                <div className="row align-items-center mb-5">
+                    <div className="col-md-4">
+                        <img src={image} className="img-fluid" alt="" />
+                    </div>
+                    <div className="col-md-8">
+                        <p style={{ fontSize: "19px", lineHeight: "31px" }}>
+                            {description}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleService;
